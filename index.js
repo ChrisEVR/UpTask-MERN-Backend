b5e9abe5498988b87a0fa7312d34029791b2dd7f
ChrisEVR <christianvargasraudales@gmail.com>
2022-03-31 00:15:27 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from 'dotenv'
+import conectarDB from "./config/db.js"
+import usuarioRoutes from "./routes/usuarioRoutes.js"
+import proyectoRoutes from "./routes/proyectoRoutes.js"
+
+const app = express()
+
+dotenv.config()
+
+conectarDB()
+
+//Routing
+app.use("/api/usuarios", usuarioRoutes)
+app.use("/api/proyectos", proyectoRoutes)
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
